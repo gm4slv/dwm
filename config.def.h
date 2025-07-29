5a35a625b4e2cff7a4216112eada39d182226059
John Pumford-Green <john@gm4slv.plus.com>
2025-07-29 20:39:44 +0100

    config updated

--- config.def.h
+++ config.def.h
@@ -3,26 +3,27 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating  = 0;  /*1 means swallow floating windows by default*/
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int user_bh            = 30;        /* 0 measn that dwm will calculate bar, =>1 means dwm will user_bh as bar height */
+static const char *fonts[]          = { "DejaVuSansMono NFM:size=12:style=Book" };
+static const char dmenufont[]       = "DejaVuSansMono NFM:size=12:style=Book";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#c4a000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_yellow, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow },
 };
 
 /* tagging */
@@ -33,17 +34,20 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+
+       /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+       { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+       { "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+       { "URxvt",       NULL,     NULL,           0,         0,          1,           0,        -1 }, 
+       { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -69,23 +73,38 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-g", "120x24", NULL, NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	             XK_Return, spawn,          {.v = termcmd } },
+
+	/* change to MOD-Return */
+	/*{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },*/
+	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+
+	/* change to MOD-b */
+	/*{ MODKEY|ShiftMask,             XK_f, 	   spawn,          {.v = firefoxcmd } },*/
+	{ MODKEY,             XK_b, 	   spawn,          {.v = firefoxcmd } },
+	
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Down,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Up,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
+
+	/* change to MOD-shift-Return */
+	/*{ MODKEY,                       XK_Return, zoom,           {0} },*/
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY, 	                XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
