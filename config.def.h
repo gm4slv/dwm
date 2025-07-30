a9f6f3297ef24114c2af2732724f3feaa5ec69c3
John Pumford-Green <john@gm4slv.plus.com>
2025-07-30 23:26:54 +0100

    bar to top again

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 35;        /* 0 measn that dwm will calculate bar, =>1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "DejaVuSansMono NFM:size=12:style=Book" };
 static const char dmenufont[]       = "DejaVuSansMono NFM:size=12:style=Book";
